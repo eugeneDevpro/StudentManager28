2c3581a3f4c5c3645be08a6cd0457c36d8a50e7d
007 <extrasoft01@gmail.com>
2022-08-29 19:11:14 +0300

    master

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,8 +1,7 @@
 package constants;
 
 public interface Constants {
-// local
-//    String CONNECTION_URL = "jdbc:mysql://localhost:3306/students_28_29?user=root&password=ofwgfta12";
+
 // remote
 String CONNECTION_URL = "jdbc:mysql://localhost:3306/students_28_29?user=root19&password=root19011994";
 
